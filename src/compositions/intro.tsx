40ce7ec942f13f160a3e77df9ea68008d119ac2c
xvoorvaa <xander@xvproductions.com>
2023-07-13 12:42:33 +0200

    Changed font and added motion blur

--- src/compositions/intro.tsx
+++ src/compositions/intro.tsx
@@ -6,19 +6,20 @@ import {
   interpolate,
   Img,
 } from 'remotion';
+import { CameraMotionBlur } from '@remotion/motion-blur';
 import { Session as SessionType } from '../types';
 import SpringIn from '../components/ZoomIn';
 import MoveObject from '../components/MoveObject';
 
 // CONSTANTS - Please change these to change the theme of the animation
-import { loadFont } from '@remotion/google-fonts/Inter';
+import { loadFont } from '@remotion/google-fonts/Vollkorn';
 const LEFT_COLOUR = '#00F839';
 const RIGHT_COLOUR = '#0DFBFF';
 const LOGO_PICTURE = '/images/FtC.svg';
 const SCALE_IMAGE = 2;
 
 const { fontFamily } = loadFont('normal', {
-  weights: ['200', '400'],
+  weights: ['400', '600'],
 });
 
 interface Props {
@@ -57,87 +58,90 @@ export function Intro(props: Props) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   return (
-    <AbsoluteFill style={{ opacity }}>
-      <Sequence name="Background">
-        <AbsoluteFill
-          style={{
-            background: `linear-gradient(to right, ${LEFT_COLOUR}, ${RIGHT_COLOUR})`,
-          }}
-        />
-      </Sequence>
-
-      <Sequence durationInFrames={70}>
-        <AbsoluteFill>
-          <div
-            style={{ transform: `scale(${SCALE_IMAGE})` }}
-            className="flex items-center justify-center h-screen"
-          >
-            <SpringIn image={LOGO_PICTURE} />
-          </div>
-        </AbsoluteFill>
-      </Sequence>
-
-      <Sequence from={70}>
-        <AbsoluteFill>
-          <div
-            style={{ transform: `scale(${SCALE_IMAGE})` }}
-            className="flex items-center justify-center h-screen"
-          >
-            <MoveObject x={600 / SCALE_IMAGE} y={0} durationInSeconds={0.5}>
-              <Img src={staticFile(LOGO_PICTURE)} />
-            </MoveObject>
-          </div>
-        </AbsoluteFill>
-      </Sequence>
-
-      <Sequence name="Speaker info" from={70}>
-        <AbsoluteFill>
-          <div
+    <CameraMotionBlur samples={5}>
+      <AbsoluteFill style={{ opacity }}>
+        <Sequence name="Background">
+          <AbsoluteFill
             style={{
-              transform: 'translateY(200px) translateX(-1000px)',
+              background: `linear-gradient(to right, ${LEFT_COLOUR}, ${RIGHT_COLOUR})`,
             }}
-          >
-            {props.session.speakers.map((speaker, index) => (
-              <div
-                key={index}
-                style={{
-                  transform: `translateY(${300 + index * -75}px)`,
-                  fontSize: 60,
-                  fontFamily: 'Arial',
-                }}
-              >
-                <Sequence
-                  key={index}
-                  name={`Speaker: ${speaker.name}`}
-                  durationInFrames={durationInFrames}
-                >
-                  <MoveObject x={1150} y={0} durationInSeconds={0.8}>
-                    <div>{speaker.name}</div>
-                  </MoveObject>
-                </Sequence>
-              </div>
-            ))}
-            <Sequence name="Session name" durationInFrames={durationInFrames}>
-              <MoveObject x={1150} y={0} durationInSeconds={0.6}>
+          />
+        </Sequence>
+
+        <Sequence durationInFrames={70}>
+          <AbsoluteFill>
+            <div
+              style={{ transform: `scale(${SCALE_IMAGE})` }}
+              className="flex items-center justify-center h-screen"
+            >
+              <SpringIn image={LOGO_PICTURE} />
+            </div>
+          </AbsoluteFill>
+        </Sequence>
+
+        <Sequence from={69}>
+          <AbsoluteFill>
+            <div
+              style={{ transform: `scale(${SCALE_IMAGE})` }}
+              className="flex items-center justify-center h-screen"
+            >
+              <MoveObject x={600 / SCALE_IMAGE} y={0} durationInSeconds={0.5}>
+                <Img src={staticFile(LOGO_PICTURE)} />
+              </MoveObject>
+            </div>
+          </AbsoluteFill>
+        </Sequence>
+
+        <Sequence name="Speaker info" from={70}>
+          <AbsoluteFill>
+            <div
+              style={{
+                transform: 'translateY(200px) translateX(-1000px)',
+              }}
+            >
+              {props.session.speakers.map((speaker, index) => (
                 <div
+                  key={index}
                   style={{
-                    transform: 'translateY(400px)',
+                    transform: `translateY(${300 + index * -75}px)`,
+                    fontSize: 60,
                     fontFamily,
-                    fontWeight: '200',
-                    fontSize: 40,
+                    fontWeight: 600,
                   }}
                 >
-                  {splitTextIntoLines(props.session.name, 30).map(
-                    (line, index) => (
-                      <div key={index}>{line}</div>
-                    ),
-                  )}
+                  <Sequence
+                    key={index}
+                    name={`Speaker: ${speaker.name}`}
+                    durationInFrames={durationInFrames}
+                  >
+                    <MoveObject x={1150} y={0} durationInSeconds={0.8}>
+                      <div>{speaker.name}</div>
+                    </MoveObject>
+                  </Sequence>
                 </div>
-              </MoveObject>
-            </Sequence>
-          </div>
-        </AbsoluteFill>
-      </Sequence>
-    </AbsoluteFill>
+              ))}
+              <Sequence name="Session name" durationInFrames={durationInFrames}>
+                <MoveObject x={1150} y={0} durationInSeconds={0.6}>
+                  <div
+                    style={{
+                      transform: 'translateY(400px)',
+                      fontFamily,
+                      fontWeight: '200',
+                      fontSize: 40,
+                    }}
+                  >
+                    {splitTextIntoLines(props.session.name, 30).map(
+                      (line, index) => (
+                        <div key={index}>{line}</div>
+                      ),
+                    )}
+                  </div>
+                </MoveObject>
+              </Sequence>
+            </div>
+          </AbsoluteFill>
+        </Sequence>
+      </AbsoluteFill>
+    </CameraMotionBlur>
   );
 }
